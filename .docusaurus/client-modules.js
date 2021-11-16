5cbcd17cea9c6914b94ccfd07341b6090b48f499
iantfrench <ian.french@1kosmos.com>
2021-11-16 09:11:57 -0800

    initial commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/ianfrench/Downloads/devx-front-tohit/docusaurus2-netlifycms/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/ianfrench/Downloads/devx-front-tohit/docusaurus2-netlifycms/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/ianfrench/Downloads/devx-front-tohit/docusaurus2-netlifycms/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/ianfrench/Downloads/devx-front-tohit/docusaurus2-netlifycms/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/ianfrench/Downloads/devx-front-tohit/docusaurus2-netlifycms/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
+  require("/Users/ianfrench/Downloads/devx-front-tohit/docusaurus2-netlifycms/src/css/custom.css"),
+];
